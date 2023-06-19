48d7a79cafc2ce4259f25f490a79182f35dea53e
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-19 22:24:22 +0100

    moved card function

--- src/components/movies.svelte
+++ src/components/movies.svelte
@@ -1,1 +1,15 @@
+<script>
+	import MovieCard from "./cards/MovieCard.svelte";
+
+    export let movies
+
+</script>
 <h3>Movies</h3>
+<div>
+    <div>
+{#each movies as movie}
+    <MovieCard />
+
+{/each}
+    </div>
+</div>
