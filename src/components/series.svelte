41411855f0ba460be85e80015022bee9cf9caa38
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-20 22:13:54 +0100

    series page component

--- src/components/series.svelte
+++ src/components/series.svelte
@@ -1,1 +1,38 @@
+<script context = "module">
+		import SeriesCard from "./cards/SeriesCard.svelte";
+
+export async function load({fetch}){
+const res = fetch(`//add req`)
+const data = await  res.json()
+if (res.ok){
+    return{
+        props: { req: data.results}
+    }
+}
+}
+
+export let series
+</script>
+
+
 <h3>Series</h3>
+<div>
+    <div>
+{#each series as series}
+    <SeriesCard />
+
+{/each}
+    </div>
+</div>
+
+
+<script>
+
+    export let req
+</script>
+
+
+<Series series = {req}/>
+
+
+
