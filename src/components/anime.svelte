5de0facc4086ebbd0abfc93c51cb1b79333d3a34
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-20 22:12:54 +0100

    anime page component

--- src/components/anime.svelte
+++ src/components/anime.svelte
@@ -1,1 +1,35 @@
+<script context = "module">
+		import AnimeCard from "./cards/AnimeCard.svelte";
+
+export async function load({fetch}){
+const res = fetch(`//add req`)
+const data = await  res.json()
+if (res.ok){
+    return{
+        props: { req: data.results}
+    }
+}
+}
+
+export let animes
+</script>
+
+
 <h3>Anime</h3>
+<div>
+    <div>
+{#each animes as anime}
+    <AnimeCard />
+
+{/each}
+    </div>
+</div>
+
+
+<script>
+
+    export let req
+</script>
+
+<Anime anime = {req}/>
+
