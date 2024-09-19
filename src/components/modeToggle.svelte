ec54e2f79e2d0995c1debb26c6dea85a1df3f8ea
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-09-19 17:04:33 +0100

    refactor modeToggle component to use external toggleMode function and update button styles

--- src/components/modeToggle.svelte
+++ src/components/modeToggle.svelte
@@ -1,22 +1,16 @@
 <script>
   import { Button } from "$lib/components/ui/button";
   import { Moon, Sun } from "svelte-lucide";
-  import { writable } from "svelte/store";
 
-  // Create a writable store for the theme
-  const theme = writable("light");
-
-  // Function to toggle the theme
-  const toggleTheme = () => {
-    theme.update(current => current === "dark" ? "light" : "dark");
-  };
+ import { toggleMode } from "mode-watcher";
 </script>
-
-<Button variant="ghost" class="w-9 px-0" on:click={toggleTheme}>
-  {#if $theme === "dark"}
-    <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-  {:else}
-    <Moon class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-  {/if}
-  <span class="sr-only">Toggle theme</span>
+ 
+<Button on:click={toggleMode} variant="outline" size="icon">
+ <Sun
+  class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+ />
+ <Moon
+  class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+ />
+ <span class="sr-only">Toggle theme</span>
 </Button>
