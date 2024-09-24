7235641f7f37032016f67c35f9511f5b34356c46
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-09-24 11:02:51 +0100

    chore: update Svelte adapter to use Vercel and add to devDependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { sveltekit } from '@sveltejs/kit/vite';
 import dotenv from 'dotenv';
